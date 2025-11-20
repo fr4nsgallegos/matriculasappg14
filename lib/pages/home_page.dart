ae377c55c57afac388913a5ead03f09dce11d3e9
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-11-19 20:32:25 -0500

    creando matriculas

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:matriculasappg14/models/carrera_model.dart';
+import 'package:matriculasappg14/models/matricula_model.dart';
 import 'package:matriculasappg14/models/persona_model.dart';
+import 'package:matriculasappg14/models/universidad_model.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,6 +11,22 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<PersonaModel> personas = [];
+  List<Widget> listar(List<PersonaModel> personas) {
+    return personas.map((persona) {
+      return ListTile(
+        title: Text(persona.nombre),
+        subtitle: Text(persona.apellido),
+      );
+    }).toList();
+  }
+
+  UniversidadModel tecsup = UniversidadModel(
+    nombre: "TECSUP",
+    direccion: "AV 1234 LIMA",
+    ruc: "123456789987",
+    telefono: "9876543212",
+    matriculas: [],
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -16,19 +35,47 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                personas.add(
-                  PersonaModel(
-                    nombre: "Jhonny",
-                    apellido: "Galleogos",
-                    direccion: "Av 123 123",
-                  ),
-                );
-                setState(() {});
-              },
-              child: Text("Agregar persona"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+
+              children: [
+                Text("${tecsup.nombre} - ${tecsup.matriculas.length}"),
+                ElevatedButton(
+                  onPressed: () {
+                    PersonaModel jhonnyEstudiante = PersonaModel(
+                      nombre: "Jhonny",
+                      apellido: "Galleogos",
+                      direccion: "Av 123 123",
+                    );
+                    CarreraModel carreraSoftware = CarreraModel(
+                      "Ing. Software",
+                      "5 años",
+                    );
+                    tecsup.matriculas.add(
+                      MatriculaModel(
+                        fecha: "19/11/25",
+                        hora: "15:30",
+                        estudiante: jhonnyEstudiante,
+                        carrera: carreraSoftware,
+                      ),
+                    );
+                    setState(() {});
+                  },
+                  child: Text("Agregar matrícula"),
+                ),
+              ],
             ),
+
+            ...tecsup.matriculas.map((matricula) {
+              return ListTile(
+                title: Text(
+                  "${matricula.estudiante.nombre} ${matricula.estudiante.apellido}",
+                ),
+                subtitle: Text(
+                  "${matricula.carrera.nombre} - ${matricula.carrera.duracion}",
+                ),
+              );
+            }).toList(),
           ],
         ),
       ),
