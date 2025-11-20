f853eb551073b016445d30d8b0e7d17888b78c5c
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-11-19 20:00:59 -0500

    como hago para mostrar esta lista personas en listile debajo del botón

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:matriculasappg14/models/persona_model.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<PersonaModel> personas = [];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                personas.add(
+                  PersonaModel(
+                    nombre: "Jhonny",
+                    apellido: "Galleogos",
+                    direccion: "Av 123 123",
+                  ),
+                );
+                setState(() {});
+              },
+              child: Text("Agregar persona"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
